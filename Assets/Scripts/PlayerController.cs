9e8f58c32764e66fd6d29fdebfbca7783203b592
ryankenney42 <ryankenney42@hotmail.com>
2016-10-17 11:19:19 +0200

    Jumping minus ground check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,12 +4,24 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+	public float jumpForce = 1000f;
+	[HideInInspector] public bool jump = false;
 
 	private Rigidbody rb;
+	private bool notGrounded = false;
 
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update()
+	{
+		/*grounded = Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer (currentPage));
+		*/
+		if (Input.GetKeyDown ("space")/* && notGrounded*/) {
+			jump = true;
+		}
 	}
 
 	void FixedUpdate ()
@@ -20,5 +32,13 @@ public class PlayerController : MonoBehaviour {
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
 		rb.AddForce (movement * speed);
+
+		if (jump) {
+			rb.AddForce (new Vector2 (0f, jumpForce));
+			jump = false;
+			/*notGrounded = true*/;
+		}
+
+
 	}
 }
