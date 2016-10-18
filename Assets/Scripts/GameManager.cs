2e233b61699eca7675af74bf77812ed7bcf3c7b0
jcl240 <jcl240@georgetown.edu>
2016-10-18 16:38:32 +0200

    added main menu and gameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,25 +11,17 @@ public class GameManager : MonoBehaviour {
 	private static GameManager _instance;
 	public static float deathHeight;
 
-	GameObject punchObject;
-	public Text punchesText;
 	GameObject loseGameGraphics;
 
 	public static GameState State = GameState.Playing;
 
 	public static float timeStarted;
-	public int punches;
 	private static int points;
 
 	void Awake(){
 		State = GameState.Playing;
 		deathHeight = gameObject.transform.localPosition.y;
 		_instance = this;
-		_instance.punchObject = GameObject.Find ("/Canvas/number");
-		if (_instance.punchObject != null) {
-			_instance.punchesText = _instance.punchObject.GetComponent<Text> ();
-			_instance.punchesText.text = punches.ToString ();
-		}
 		_instance.loseGameGraphics = GameObject.Find ("/Canvas/LoseGraphics");
 		if (_instance.loseGameGraphics != null)
 			_instance.loseGameGraphics.SetActive(false);
@@ -40,21 +32,13 @@ public class GameManager : MonoBehaviour {
 	}
 
 	public void setPoints(){
-		GameObject.Find ("/Canvas/coinsText").GetComponent<Text>().text = points.ToString();
+		if (GameObject.Find ("/Canvas/coinsText"))
+			GameObject.Find ("/Canvas/coinsText").GetComponent<Text>().text = points.ToString();
 	}
 
 	public static void AddPoints(){
 		points += 1;
 		_instance.setPoints ();
-	}
-
-	public void removePunch(){
-		_instance.punches -= 1;
-		_instance.punchesText.text = _instance.punches.ToString();
-	}
-
-	public static bool hasPunches(){
-		return _instance.punches != 0;
 	}
 
 	public void LoseTheGame(){
